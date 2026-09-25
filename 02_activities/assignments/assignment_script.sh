c959ee65adec515cdbcd35da6615cc557476453b
ramaedaibis <ramaedaibis@gmail.com>
2026-09-25 14:24:59 -0400

    adding coworker's comments

--- 02_activities/assignments/assignment_script.sh
+++ 02_activities/assignments/assignment_script.sh
@@ -52,12 +52,12 @@ cp ./data/raw/*user*.log ./data/processed/user_logs
 cp ./data/raw/*event*.log ./data/processed/event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-<<<<<<< HEAD
+#<<<<<<< HEAD
 rm ./data/raw/*ipaddr*.*
 rm ./data/processed/user_logs/*ipaddr*.*
 =======
-rf -rf ./data
->>>>>>> 93b0ce62c8e1521dc6a70f3f9dbd56faa69ebad7
+rm -rf ./data
+#>>>>>>> 93b0ce62c8e1521dc6a70f3f9dbd56faa69ebad7
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 touch ./data/inventory.txt
